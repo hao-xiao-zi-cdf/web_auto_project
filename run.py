e2d810e9051269cef454a5fd0f4c5d04c45cf85d
hao_xiao_zi <xh483817@gmail.com>
2026-08-15 17:01:11 +0800

    多人操作测试脚本编写、pytest命令修改

--- run.py
+++ run.py
@@ -2,7 +2,17 @@ import os
 import pytest
 
 if __name__ == '__main__':
-    # 运行测试用例
-    pytest.main(['--alluredir', './reports'])
+    # 清理历史报告
+    if os.path.exists('./allure_report'):
+        import shutil
+
+        shutil.rmtree('./allure_report')
+
+    # 运行测试用例(自动清理 allure-results)
+    pytest.main(['--alluredir', './reports', '--clean-alluredir'])
+
     # 生成测试报告
     os.system('allure generate ./reports -o ./allure_report --clean')
+
+    # 打开报告
+    os.system('allure open ./allure_report')
